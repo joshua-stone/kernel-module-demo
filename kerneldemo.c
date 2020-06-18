e96af45102bebd6006453b9b2febe3d09e3a6658
Joshua Stone <jstone@joshua-laptop.lan>
2020-06-17 20:05:37 -0400

    Add initial commit

---
+++ kerneldemo.c
@@ -0,0 +1,24 @@
+#include <linux/module.h> 
+#include <linux/kernel.h>
+#include <linux/init.h>
+
+static int __init hello_start(void) 
+{ 
+	printk(KERN_INFO "Hello world!\n"); 
+	return 0; 
+} 
+
+static void __exit hello_end(void) 
+{ 
+	printk(KERN_INFO "Goodbye World!\n"); 
+} 
+
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Joshua Stone");
+MODULE_DESCRIPTION("Hello World kernel module");
+MODULE_VERSION("0.1");
+
+module_init(hello_start); 
+module_exit(hello_end); 
+
